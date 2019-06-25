d4c7878225f011be2e73757969b1a1110146be7d
Dumas Carvalho <dumasmcarvalho@gmail.com>
2019-06-25 00:35:42 -0300

    Melhorado Script de Exclusão

---
+++ excluirUmUsuarioOuGrupo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+
+echo -e "Deseja remover um usuário ou um grupo?\n 1) Remover um usuário;\n 2) Remover um grupo.\n"
+
+read -p "Escolha uma das alternativas acima: " escolha
+
+echo "";
+
+if [ $escolha -eq 1 ] ; then
+	read -p "Informe o nome do usuário para excluí-lo: " usuario
+
+	smbpasswd -x $usuario &> /dev/null && userdel -r $usuario &> /dev/null
+
+	if [ $? -eq 0 ] ; then
+        	echo -e "\nUsuário excluído com sucesso!"
+	else
+        	echo -e "\nO usuário informado já foi excluído ou não existe."
+	fi
+elif [ $escolha -eq 2 ] ; then
+        read -p "Informe o nome do grupo para excluí-lo: " grupo
+
+        groupdel $grupo &> /dev/null
+
+        if [ $? -eq 0 ] ; then
+                echo -e "\nGrupo excluído com sucesso!"
+        else
+                echo -e "\nO grupo informado já foi excluído ou não existe."
+        fi
+else
+	echo "Opção inválida, favor tentar novamente!"
+fi
